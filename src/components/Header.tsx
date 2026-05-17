7d8276a2514fa49a95a2cf06a6616449842ad8ed
PantelisTs <pantsotsgr@gmail.com>
2026-05-17 18:46:26 +0300

    fix: edit logo on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <header className="bg-ex1-blue fixed w-full py-4">
-            <div className="container mx-auto flex items-center justify-between">
+        <header className="bg-ex1-blue fixed w-full py-4 text-center">
+            <div className="container mx-auto">
 
-                <a href="/" className="text-white text-xl font-bold whitespace-nowrap border py-1 px-2 rounded-md ">
+                <a href="/" className="text-white text-xl font-bold whitespace-nowrap border py-1 px-2 rounded-md">
                     FORM ID
                 </a>
             </div>
